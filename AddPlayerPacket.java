6c72ab12ab6bfdaad599277fc0d554df0e924529
Syllabas <100375121+Syllabas@users.noreply.github.com>
2022-03-17 19:27:08 +0100

    Create AddPlayerPacket.java

---
+++ AddPlayerPacket.java
@@ -0,0 +1,13 @@
+package serveur;
+
+import java.io.Serializable;
+
+public class AddPlayerPacket implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	public int id;
+	public String name;
+	
+
+}
